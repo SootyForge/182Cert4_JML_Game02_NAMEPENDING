95887d864968982555ee81cb0fd8ea3ce45cb759
SootyForge <42379545+SootyForge@users.noreply.github.com>
2018-09-18 15:47:43 +1000

    Corrected Update() to LateUpdate()

--- Assets/Game02_NAMEPENDING/Scripts/CameraController.cs
+++ Assets/Game02_NAMEPENDING/Scripts/CameraController.cs
@@ -51,7 +51,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         #region Camera Rotation
         if (target)
